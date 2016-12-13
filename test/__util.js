5a66d130721d9c34f3c6aa81b0b9b0e6987e9696
sdvinov <odvakr.wikia@gmail.com>
2016-12-12 22:30:23 -0500

    fixed tests

--- test/__util.js
+++ test/__util.js
@@ -5,14 +5,22 @@ const sinon = require('sinon');
 
 describe('Utility tool', () => {
   beforeEach(() => {
+    // Before each test puts spy on console.log
+    // sinon.spy(object, method)
     this.console = {
-      log: sinon.spy(),
+      log: sinon.spy(console, 'log'),
     };
   });
 
-  it('test', () => {
-    const _this = this;
+  afterEach(() => {
+    // Restores functionallity of console.log after each test
+    console.log.restore();
+  });
+
+  it('Testing console.log', (done) => {
+    // Calling the function and expecting it to be called
     util.debug('Server is running on port 3000', './../src/server', 'n');
-    expect(_this.console.log.callCount).to.equal(1);
+    expect(console.log).to.be.called;
+    done();
   });
 });
